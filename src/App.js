cceb176fd04b2960f402be2fbe0718d40bbab78e
YxngShifu <mimadami01@gmail.com>
2022-10-28 00:17:21 +0800

    Added buttons to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import LinkButton from "./components/LinkButton" 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LinkButton
+       name='Twitter Link'
+       link='https://twitter.com/Yxng_shifu'/>
+       <LinkButton
+       name='Zuri Team'
+       link='https://training.zuri.team/'
+       />
+       <LinkButton
+       name='Zuri Books'
+       link='https://books.zuri.team'
+       />
+       <LinkButton
+       name='Python Books'
+       link='https://books.zuri.team/python-for-begginners?ref_id=Yxng_Shifu'
+       />
+       <LinkButton
+       name='Background check for coders'
+       link='https://background.zuri.team'
+       />
+       <LinkButton
+       name='Design Books'
+       link='https://books.zuri.team/design-rules'
+       />
+       
     </div>
   );
 }
--- src/components/TextBanner.js
+++ src/components/TextBanner.js
@@ -1,9 +1,17 @@
 import React from "react"
 
 const TextBanner =()=>{
+    const{
+        title,
+        text
+    }=props
 
 
  return(
+    <div>
+        <h2>{title}</h2>
+        <p>{text}</p>
+    </div>
 
  )
 
